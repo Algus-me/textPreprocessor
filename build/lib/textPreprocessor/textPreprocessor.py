704b3654d10f6d936fe8587492145f3cf7aa3b06
Algu <alex.gusarin@gmail.com>
2019-04-10 09:37:38 +0300

    changed inner structure

--- build/lib/textPreprocessor/textPreprocessor.py
+++ build/lib/textPreprocessor/textPreprocessor.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+from nltk.corpus import stopwords
 import spacy
 import pymorphy2
 from polyglot.detect import Detector
@@ -10,85 +11,60 @@ import serializableClass
 from .textTokenizer import TextTokenizerSpacyBased
 from .textTokenizer import TextTokenizerRus
 
-def initLanguagePreprocessorComponents():
+initializeFollowingLanguageProcessors = ["un", "en", "de", "es", "pt", "fr", "it", "nl", "ru"]
 
-    languagePreprocessorComponents = {}
-    
-    languagePreprocessorComponents["un"] = {}
-    languagePreprocessorComponents["un"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["un"]["tokenizer"].setLinguisticProcessor(spacy.load('xx'))
-    languagePreprocessorComponents["un"]["stopPos"] = set()
-    languagePreprocessorComponents["un"]["stopTokens"] = set()
-    languagePreprocessorComponents["un"]["stopLemmas"] = set()
+def initLanguageTokenizers(listOfSupportedLanguages):
+    languageTokenizers = {}
 
-    languagePreprocessorComponents["en"] = {}
-    languagePreprocessorComponents["en"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["en"]["tokenizer"].setLinguisticProcessor(spacy.load('en'))
-    languagePreprocessorComponents["en"]["stopPos"] = set()
-    languagePreprocessorComponents["en"]["stopTokens"] = set()
-    languagePreprocessorComponents["en"]["stopLemmas"] = set()
+    for lang in listOfSupportedLanguages:
+        if lang == "un":
+            languageTokenizers["un"] = TextTokenizerSpacyBased()
+            languageTokenizers["un"].setLinguisticProcessor(spacy.load('xx'))
+        elif lang == "ru":
+            languageTokenizers["ru"] = TextTokenizerRus()
+        else:
+            languageTokenizers[lang] = TextTokenizerSpacyBased()
+            languageTokenizers[lang].setLinguisticProcessor(spacy.load(lang))
 
-    languagePreprocessorComponents["de"] = {}
-    languagePreprocessorComponents["de"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["de"]["tokenizer"].setLinguisticProcessor(spacy.load('de'))
-    languagePreprocessorComponents["de"]["stopPos"] = set()
-    languagePreprocessorComponents["de"]["stopTokens"] = set()
-    languagePreprocessorComponents["de"]["stopLemmas"] = set()
+    return languageTokenizers
 
-    languagePreprocessorComponents["es"] = {}
-    languagePreprocessorComponents["es"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["es"]["tokenizer"].setLinguisticProcessor(spacy.load('es'))
-    languagePreprocessorComponents["es"]["stopPos"] = set()
-    languagePreprocessorComponents["es"]["stopTokens"] = set()
-    languagePreprocessorComponents["es"]["stopLemmas"] = set()
+def getParametersDict():
+    parametersDict = {}
+    parametersDict["caseSensitive"] = False
+    parametersDict["stopPos"] = set()
+    parametersDict["stopTokens"] = set()
+    parametersDict["stopTokensLower"] = set()
+    parametersDict["stopLemmas"] = set()
 
-    languagePreprocessorComponents["pt"] = {}
-    languagePreprocessorComponents["pt"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["pt"]["tokenizer"].setLinguisticProcessor(spacy.load('pt'))
-    languagePreprocessorComponents["pt"]["stopPos"] = set()
-    languagePreprocessorComponents["pt"]["stopTokens"] = set()
-    languagePreprocessorComponents["pt"]["stopLemmas"] = set()
+    return parametersDict
 
-    languagePreprocessorComponents["fr"] = {}
-    languagePreprocessorComponents["fr"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["fr"]["tokenizer"].setLinguisticProcessor(spacy.load('fr'))
-    languagePreprocessorComponents["fr"]["stopPos"] = set()
-    languagePreprocessorComponents["fr"]["stopTokens"] = set()
-    languagePreprocessorComponents["fr"]["stopLemmas"] = set()
+def initLanguageTokenizeParameters(listOfSupportedLanguages):
+    languageTokenizeParameters = {}
 
-    languagePreprocessorComponents["it"] = {}
-    languagePreprocessorComponents["it"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["it"]["tokenizer"].setLinguisticProcessor(spacy.load('it'))
-    languagePreprocessorComponents["it"]["stopPos"] = set()
-    languagePreprocessorComponents["it"]["stopTokens"] = set()
-    languagePreprocessorComponents["it"]["stopLemmas"] = set()
+    for lang in listOfSupportedLanguages:
+        languageTokenizeParameters[lang] = getParametersDict()
 
-    languagePreprocessorComponents["nl"] = {}
-    languagePreprocessorComponents["nl"]["tokenizer"] = TextTokenizerSpacyBased()
-    languagePreprocessorComponents["nl"]["tokenizer"].setLinguisticProcessor(spacy.load('nl'))
-    languagePreprocessorComponents["nl"]["stopPos"] = set()
-    languagePreprocessorComponents["nl"]["stopTokens"] = set()
-    languagePreprocessorComponents["nl"]["stopLemmas"] = set()
-
-    languagePreprocessorComponents["ru"] = {}
-    languagePreprocessorComponents["ru"]["tokenizer"] = TextTokenizerRus()
-    languagePreprocessorComponents["ru"]["stopPos"] = set()
-    languagePreprocessorComponents["ru"]["stopTokens"] = set()
-    languagePreprocessorComponents["ru"]["stopLemmas"] = set()
-
-    return languagePreprocessorComponents
+    return languageTokenizeParameters
 
 class TextPreprocessor(serializableClass.SerializableClass):
+    def __init__(self):
+        self.languageTokenizeParameters_ = initLanguageTokenizeParameters(initializeFollowingLanguageProcessors)
     
     def getFilteredTokens(self, text, lang):
-        tokens = self.languagePreprocessorComponents_[lang]["tokenizer"].tokenize(text.lower())
+        tokens = self.languageTokenizers_[lang].tokenize(text)
         res = []
         for t in tokens:
-            if t.pos_ in self.languagePreprocessorComponents_[lang]["stopPos"]:
+            if t.lemma_ == "-PRON-":
+                t.lemma_ = t.text.lower()
+            if t.pos_ in self.languageTokenizeParameters_[lang]["stopPos"]:
                 continue
-            if t.text in self.languagePreprocessorComponents_[lang]["stopTokens"]:
-                continue
-            if t.lemma_ in self.languagePreprocessorComponents_[lang]["stopLemmas"]:
+            if self.languageTokenizeParameters_[lang]["caseSensitive"] == False:
+                if t.text.lower() in self.languageTokenizeParameters_[lang]["stopTokensLower"]:
+                    continue
+            else:
+                if t.text in self.languageTokenizeParameters_[lang]["stopTokens"]:
+                    continue
+            if t.lemma_ in self.languageTokenizeParameters_[lang]["stopLemmas"]:
                 continue
             res.append(t)
         return res
@@ -109,16 +85,16 @@ class TextPreprocessor(serializableClass.SerializableClass):
         return lang
     
     def chooseKnownLanguageOfTextIfPossible(self, text, lang, defaultLangIfNotDetected=None):
-        if lang not in self.languagePreprocessorComponents_ or lang=="un":
+        if lang not in self.languageTokenizers_ or lang=="un":
             lettersFromText = re.sub("[\W\d_]", "", text)
             if len(lettersFromText) == 0:
                 if defaultLangIfNotDetected is not None:
                     lang = defaultLangIfNotDetected
                 else:
                     lang = "en"
-            elif re.match("^[A-Za-z]*$", text):
+            elif re.match("^[A-Za-z]*$", lettersFromText):
                 lang = "en"
-            elif re.match("^[А-Яа-я]*$", text):
+            elif re.match("^[А-Яа-я]*$", lettersFromText):
                 lang = "ru"
             elif defaultLangIfNotDetected is not None:
                 lang = defaultLangIfNotDetected
@@ -126,41 +102,63 @@ class TextPreprocessor(serializableClass.SerializableClass):
                 lang = "un"
         return lang
 
+    def setInitialParametersAutomatically(self):
+        self.setAllowedPos(["ADJ", "ADV", "INTJ", "NOUN", "PROPN", "VERB"])
+        self.setStopLemmas(set(stopwords.words("russian")), "ru")
+        self.setStopLemmas(set(stopwords.words("english")), "en")
+
 # SETTERS AND GETTERS:
 
-    def setStopPos(self, stopPos, lang):
-        self.languagePreprocessorComponents_[lang]["stopPos"] = stopPos
-    def setAllowedPos(self, allowed, lang):
-        self.languagePreprocessorComponents_[lang]["stopPos"] = self.allPos_ - allowed
+    def setCaseSensitive(self, caseSensitive, lang=None):
+        if lang is None:
+            for l in self.languageTokenizeParameters_:
+                self.languageTokenizeParameters_[l]["caseSensitive"] = caseSensitive
+        else:
+            self.languageTokenizeParameters_[lang]["caseSensitive"] = caseSensitive
+    def getCaseSensitive(self, lang):
+        return self.languageTokenizeParameters_[lang]["caseSensitive"]
+
+    def setStopPos(self, stopPos, lang=None):
+        stopPos = set(stopPos)
+        if lang is None:
+            for l in self.languageTokenizeParameters_:
+                self.languageTokenizeParameters_[l]["stopPos"] = stopPos
+        else:
+            self.languageTokenizeParameters_[lang]["stopPos"] = stopPos
+    def setAllowedPos(self, allowed, lang=None):
+        allowed = set(allowed)
+        if lang is None:
+            for l in self.languageTokenizeParameters_:
+                self.languageTokenizeParameters_[l]["stopPos"] = self.allPos_ - allowed
+        else:
+            self.languageTokenizeParameters_[lang]["stopPos"] = self.allPos_ - allowed
     def getStopPos(self, lang):
-        return self.languagePreprocessorComponents_[lang]["stopPos"]
+        return self.languageTokenizeParameters_[lang]["stopPos"]
 
     def setStopTokens(self, stopTokens, lang):
-        self.languagePreprocessorComponents_[lang]["stopTokens"] = stopTokens
+        self.languageTokenizeParameters_[lang]["stopTokens"] = stopTokens
+        for st in stopTokens:
+            self.languageTokenizeParameters_[lang]["stopTokens"].add(st.lower())
     def getStopTokens(self, lang):
-        return self.languagePreprocessorComponents_[lang]["stopTokens"]
+        return self.languageTokenizeParameters_[lang]["stopTokens"]
 
     def setStopLemmas(self, stopLemmas, lang, normalize=False):
         if normalize == True:
             normalized = []
             for sl in stopLemmas:
                 morphRes = self.morph_.parse(sl)[0]
-                normalized.append(morphRes.normal_form)
+                normalized.append(morphRes.normal_form.lower())
             stopLemmas = set(normalized)
-        self.languagePreprocessorComponents_[lang]["stopLemmas"] = stopLemmas
+        else:
+            stopLemmas = set([sl.lower() for sl in stopLemmas])
+        self.languageTokenizeParameters_[lang]["stopLemmas"] = stopLemmas
     def getStopLemmas(self, lang):
-        return self.languagePreprocessorComponents_[lang]["stopLemmas"]
+        return self.languageTokenizeParameters_[lang]["stopLemmas"]
 
 # SAVE AND LOAD:
 
     def saveThisObjectDataOnly_(self, folder):
-        dataToSave = {}
-        for lang in self.languagePreprocessorComponents_:
-            dataToSave[lang] = {}
-            for comp in self.languagePreprocessorComponents_[lang]:
-                if comp == "tokenizer":
-                    continue
-                dataToSave[lang][comp] = list(self.languagePreprocessorComponents_[lang][comp])
+        dataToSave = self.changeSetsToListsInLanguageTokenizeParametersDict(self.languageTokenizeParameters_)
         with open(os.path.join(folder, "data.json"), "w") as fp:
             json.dump(dataToSave, fp)
 
@@ -168,14 +166,46 @@ class TextPreprocessor(serializableClass.SerializableClass):
         dataToLoad = None
         with open(os.path.join(folder, "data.json"), "r") as fp:
             dataToLoad = json.load(fp)
-        for lang in dataToLoad:
-            for comp in dataToLoad[lang]:
-                self.languagePreprocessorComponents_[lang][comp] = set(dataToLoad[lang][comp])
+
+        self.languageTokenizeParameters_ = self.changeListsToSetsInLanguageTokenizeParametersDict(dataToLoad)
+
+        for lang in initializeFollowingLanguageProcessors:
+            if lang not in self.languageTokenizeParameters_:
+                self.languageTokenizeParameters_[lang] = getParametersDict()
+
+# PRIVATE FUNCTIONS:
+
+    def changeSetsToListsInLanguageTokenizeParametersDict(self, languageTokenizeParameters):
+        res = {}
+        for lang in languageTokenizeParameters:
+            res[lang] = {}
+            for param in languageTokenizeParameters[lang]:
+                if type(languageTokenizeParameters[lang][param]) == set:
+                    res[lang][param] = list(languageTokenizeParameters[lang][param])
+                else:
+                    res[lang][param] = languageTokenizeParameters[lang][param]
+
+        return res
+
+    def changeListsToSetsInLanguageTokenizeParametersDict(self, languageTokenizeParameters):
+        res = {}
+        for lang in languageTokenizeParameters:
+            res[lang] = {}
+            for param in languageTokenizeParameters[lang]:
+                if type(languageTokenizeParameters[lang][param]) == list:
+                    res[lang][param] = set(languageTokenizeParameters[lang][param])
+                else:
+                    res[lang][param] = languageTokenizeParameters[lang][param]
+
+        return res
 
 # FIELDS:
 
-    languagePreprocessorComponents_ = initLanguagePreprocessorComponents()
+    languageTokenizers_ = initLanguageTokenizers(initializeFollowingLanguageProcessors)
 
+    languageTokenizeParameters_ = None
+
+    # https://universaldependencies.org/u/pos/index.html
     allPos_ = set(["ADJ", "ADV", "INTJ", "NOUN", "PROPN", "VERB",
                    "ADP", "AUX", "CCONJ", "DET", "NUM", "PART",
                    "PRON", "SCONJ", "PUNCT", "SYM", "X"])
